4e4b20dc5407321abffc1452d7a7e8a47c33bc1d
Mike Morton <mmorton7295@gmail.com>
2021-07-31 14:51:02 -0500

    allows user to follow others on the User index page

--- src/views/UserIndex.vue
+++ src/views/UserIndex.vue
@@ -3,9 +3,21 @@
     <h1>User Index</h1>
     <hr />
     <div v-for="user in users" v-bind:key="user.id">
-      <!-- <p>{{ user }}</p> -->
+      <p>{{ user.id }}</p>
       <p>{{ user.name }}</p>
       <p>Total miles: {{ user.total_miles }}</p>
+      <button
+        v-if="isCurrentUserFollowing(user)"
+        v-on:click="destroyFollow(user)"
+      >
+        Unfollow
+      </button>
+      <button
+        v-if="!isCurrentUserFollowing(user)"
+        v-on:click="createFollow(user)"
+      >
+        Follow
+      </button>
       <hr />
     </div>
   </div>
@@ -25,8 +37,37 @@ export default {
   methods: {
     getUsers: function () {
       axios.get("/users").then((response) => {
-        this.users = response.data;
+        let userIndex = response.data;
+        userIndex.splice(userIndex.indexOf(parseInt(localStorage.user_id)), 1); //removes currently logged in user from list
+        this.users = userIndex;
       });
+    },
+    isCurrentUserFollowing: function (user) {
+      let user_ids = user.follower_ids.map((element) => element.id);
+      return user_ids.includes(parseInt(localStorage.user_id));
+    },
+    createFollow: function (user) {
+      axios
+        .post(`/follows/${user.id}`)
+        .then(() => {
+          user.follower_ids.push({ id: parseInt(localStorage.user_id) });
+        })
+        .catch((errors) => {
+          console.log(errors.response.data.errors);
+        });
+    },
+    destroyFollow: function (user) {
+      axios
+        .delete(`follows/${user.id}`)
+        .then(() => {
+          user.follower_ids.splice(
+            user.follower_ids.indexOf({ id: parseInt(localStorage.user_id) }),
+            1
+          );
+        })
+        .catch((errors) => {
+          console.log(errors.response.data.errors);
+        });
     },
   },
 };
