decb3e137de039c4800a1d611c265dc0c571aa00
Mike Morton <mmorton7295@gmail.com>
2021-08-01 16:45:31 -0500

    requires user login for run creation and user show pages

--- src/views/RunsNew.vue
+++ src/views/RunsNew.vue
@@ -1,33 +1,48 @@
 <template>
   <div>
     <h1>Post a Run</h1>
-    <form v-on:submit.prevent="submit()">
-      <ul v-for="error in errors" v-bind:key="error.id">
-        <li>{{ error }}</li>
-      </ul>
-      <p>
-        <label>Title: </label>
-        <input type="text" v-model="newRun.title" />
-      </p>
-      <p>
-        <label>Distance: </label>
-        <input type="text" v-model="newRun.distance" />
-      </p>
-      <p>
-        <label>Time: </label>
-        <input type="text" v-model="newRun.elapsed_time" />
-      </p>
-      <input type="submit" />
-    </form>
+    <p v-if="!loggedIn">Log in to Post</p>
+    <hr />
+    <div v-if="loggedIn">
+      <form v-on:submit.prevent="submit()">
+        <ul v-for="error in errors" v-bind:key="error.id">
+          <li>{{ error }}</li>
+        </ul>
+        <p>
+          <label>Title: </label>
+          <input type="text" v-model="newRun.title" />
+        </p>
+        <p>
+          <label>Distance: </label>
+          <input type="text" v-model="newRun.distance" />
+        </p>
+        <p>
+          <label>Time: </label>
+          <input type="text" v-model="newRun.elapsed_time" />
+        </p>
+        <input type="submit" />
+      </form>
+    </div>
   </div>
 </template>
 <script>
 import axios from "axios";
 export default {
+  props: ["checkLoggedIn"],
+  created: function () {
+    this.checkLoggedIn()
+      .then(() => {
+        this.loggedIn = true;
+      })
+      .catch(() => {
+        this.loggedIn = false;
+      });
+  },
   data: function () {
     return {
       newRun: {},
       errors: {},
+      loggedIn: false,
     };
   },
   methods: {
--- src/views/UsersShow.vue
+++ src/views/UsersShow.vue
@@ -1,31 +1,43 @@
 <template>
   <div>
-    <h1>{{ user.name }}</h1>
-    <p>Total miles: {{ user.total_miles }}</p>
-    <h3>Runs</h3>
-    <div v-for="run in user.runs" v-bind:key="run.id">
-      <p>{{ run.title }}</p>
-      <p>Distance: {{ run.distance }} miles</p>
-      <p>Time {{ run.elapsed_time }} minutes</p>
-      <br />
-      <p>Comments:</p>
-      <p v-for="comment in run.comments" v-bind:key="comment.id">
-        {{ comment.user_name }}: {{ comment.text }}
-      </p>
-      <hr />
+    <div v-if="!loggedIn">Please log in to view users</div>
+    <div v-if="loggedIn">
+      <h1>{{ user.name }}</h1>
+      <p>Total miles: {{ user.total_miles }}</p>
+      <h3>Runs</h3>
+      <div v-for="run in user.runs" v-bind:key="run.id">
+        <p>{{ run.title }}</p>
+        <p>Distance: {{ run.distance }} miles</p>
+        <p>Time {{ run.elapsed_time }} minutes</p>
+        <br />
+        <p>Comments:</p>
+        <p v-for="comment in run.comments" v-bind:key="comment.id">
+          {{ comment.user_name }}: {{ comment.text }}
+        </p>
+        <hr />
+      </div>
     </div>
   </div>
 </template>
 <script>
 import axios from "axios";
 export default {
+  props: ["checkLoggedIn"],
   data: function () {
     return {
       user: {},
+      loggedIn: false,
     };
   },
   created: function () {
-    this.getUser();
+    this.checkLoggedIn()
+      .then(() => {
+        this.getUser();
+        this.loggedIn = true;
+      })
+      .catch(() => {
+        this.loggedIn = false;
+      });
   },
   methods: {
     getUser: function () {
